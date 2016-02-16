f9b502afa0c759a4f8820ba2993dcfd7f64ca85c
Vijay Murganoor <vijaym123@gmail.com>
2016-02-15 22:07:39 -0800

    code + complete data

---
+++ tweets/parsetweets.py
@@ -0,0 +1,26 @@
+import csv
+
+def dict2xml(itemDict):
+	xml = "<tweet>\n"
+	xml += "\t<text>" + itemDict['text'] + "</text>\n"
+	xml += "\t<name>" + itemDict['user']['screen_name'] + "</name>\n"
+	return xml + "</tweet>\n"
+
+if __name__ == "__main__":
+	f = open("goldenglobes.json","r")
+	fw = open("tweets.xml","w")
+
+	xml = "<root>\n"
+	item = f.readline()
+	count = 0
+	while item:
+		itemDict = eval(item)
+		xml += dict2xml(itemDict)
+		count += 1
+		item = f.readline()
+		if count > 100:
+			break
+	xml += "</root>"
+
+	fw.write(xml)
+	fw.close()
